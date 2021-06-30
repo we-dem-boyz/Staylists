eb7a45361f704d258b2979cb4a96b0374c55e2f9
MikeAdu <mike_adusah@yahoo.com>
2021-06-29 19:34:48 -0700

    fixed the routes

---
+++ Main/models/index.js
@@ -0,0 +1,14 @@
+const User = require('./User');
+const Playlist = require('./Playlist');
+const Playlist = require('./Playlist');
+
+User.hasMany(Playlist, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Playlist.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+module.exports = { User, Playlist };
