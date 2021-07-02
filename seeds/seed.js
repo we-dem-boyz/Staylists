a1e803701881de9deef224bff662407ba96f6a89
Son Le <sle673415@gmail.com>
2021-07-01 18:41:29 -0700

    views, public

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,25 +1,25 @@
-const sequelize = require('../config/connection');
-const { User, Playlist } = require('../models');
+const sequelize = require('../config/connection')
+const { User, Playlist } = require('../models')
 
-const userData = require('./userData.json');
-const playlistData = require('./playlistData.json');
+const userData = require('./userData.json')
+const playlistData = require('./playlistData.json')
 
 const seedDatabase = async () => {
-    await sequelize.sync({ force: true });
+  await sequelize.sync({ force: true })
 
-    const users = await User.bulkCreate(userData, {
-        individualHooks: true,
-        returning: true,
-    });
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true
+  })
 
-    for (const playlist of playlistData) {
-        await Playlist.create({
-            ...playlist,
-            user_id: users[Math.floor(Math.random() * users.length)].id,
-        });
-    }
+  for (const playlist of playlistData) {
+    await Playlist.create({
+      ...playlist,
+      user_id: users[Math.floor(Math.random() * users.length)].id
+    })
+  }
 
-    process.exit(0);
-};
+  process.exit(0)
+}
 
-seedDatabase();
+seedDatabase()
