96862050b7daca0aab06bd721845b1110f863645
MikeAdu <mike_adusah@yahoo.com>
2021-07-07 22:36:22 -0700

    adding fixes

--- models/Playlist.js
+++ models/Playlist.js
@@ -1,5 +1,5 @@
-const { Model, DataTypes } = require('sequelize')
-const sequelize = require('../config/connection')
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
 class Playlist extends Model { }
 Playlist.init(
   {
@@ -21,23 +21,7 @@ Playlist.init(
       allowNull: false,
       defaultValue: DataTypes.NOW
     },
-    // songs: [
-    //   {
-    //     id: {
-    //       type: DataTypes.INTEGER,
-    //       allowNull: false,
-    //       primaryKey: true,
-    //       autoIncrement: true
-    //     },
-    //     name: {
-    //       type: DataTypes.STRING,
-    //       allowNull: false
-    //     },
-    //     genre: {
-    //       type: DataTypes.STRING,
-    //       allowNull: false
-    //     },
-    //   }],
+   
     user_id: {
       type: DataTypes.INTEGER,
       references: {
@@ -45,13 +29,7 @@ Playlist.init(
         key: 'id',
       },
     },
-    song_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'user',
-        key: 'id',
-      },
-    }
+  
   },
   {
     sequelize,
@@ -63,5 +41,5 @@ Playlist.init(
   
 );
 
-module.exports = Playlist
+module.exports = Playlist;
 
